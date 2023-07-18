7a567f6df8200898ee9c7f64dc4c5f8af7d96fdb
GP <gladkov@edu.yaconnect.com>
2023-07-18 19:16:34 +0300

    complete

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 def index(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'{name}\n{email}\n{message}')
-    return render(request, 'catalog/index.html')
+    product_list = Product.objects.all()
+    content = {
+        'object_list': product_list
+    }
+    return render(request, 'catalog/index.html', content)
+
+# def index(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         message = request.POST.get('message')
+#         print(f'{name}\n{email}\n{message}')
+#     return render(request, 'catalog/index.html')
 
 def home(request):
     if request.method == 'GET':
